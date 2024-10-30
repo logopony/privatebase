a20ec0f1575891be6327aca8b6a666fc306900da
logopony <hi@logopony.com>
2024-10-30 20:37:07 +0200

    add vscode theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cityLights from "./city-lights.json";
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: {
+        name: "city-lights",
+        type: "dark",
+        settings: cityLights.tokenColors,
+      }
+    }
+  }
 });
